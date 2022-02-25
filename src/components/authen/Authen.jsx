e25d25ae7e93b815f051a8ca3db64926f3268912
Thanh Tungg <trantungg910@gmail.com>
2022-02-25 14:23:45 +0700

    Feat: authen

---
+++ src/components/authen/Authen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Authen = () => {
+	return <div>Auten</div>;
+};
+
+export default Authen;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Nav from "../partials/Nav";
+import Search from "../search/Search";
 const Header = () => {
 	return (
-		<header className="">
+		<header className="flex justify-between py-4 px-2 bg-gray-100 shadow rounded-sm">
 			<Nav />
+			<Search />
 		</header>
 	);
 };
