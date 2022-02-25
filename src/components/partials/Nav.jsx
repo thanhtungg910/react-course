8d3ce929761d7b53e585b8b7b99a318a7a09e5ab
Thanh Tungg <trantungg910@gmail.com>
2022-02-25 14:58:51 +0700

    Fix: nav

--- src/components/partials/Nav.jsx
+++ src/components/partials/Nav.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { menu } from "../../data";
 
 const Nav = () => {
 	return (
 		<nav className="flex gap-5 items-center ">
-			<Link to="/">Home</Link>
-			<Link to="/products">San pham</Link>
-			<Link to="/contacts">Lien he</Link>
-			<Link to="/login">Login</Link>
+			{menu.map((item) => (
+				<Link key={item.id} to={item.path}>
+					{item.title}
+				</Link>
+			))}
 		</nav>
 	);
 };
---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const menu = [
+	{ id: 1, title: "Home", path: "/" },
+	{ id: 2, title: "San pham", path: "/products" },
+	{ id: 3, title: "Lien he", path: "/contacts" },
+	{ id: 4, title: "Login", path: "/login" },
+];
